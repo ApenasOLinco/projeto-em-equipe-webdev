0bb30635c3fd1e68e66927f12339272fc41a11b2
ApenasOLinco <lincoln.matos1003@gmail.com>
2024-11-22 18:13:43 -0300

    Script do swiper agora é externo
    Movido script de criação do objeto Swiper para o arquivo "scripts.js" por motivos de consistência

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,30 @@
+// Criação dos carrosséis
+document.addEventListener("DOMContentLoaded", function() {
+    const swiper = new Swiper('.swiper', {
+        loop: true,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+        },
+        on: {
+            init: function () {
+                window.dispatchEvent(new Event('resize'));
+            }
+        }
+    })
+    console.log("Created!");
+});
+
 // Obter formulário inalterado
 const formularioNovo = document.querySelector(".contact-form").innerHTML
-
 
 function clicou() {
     const formulario = document.querySelector(".contact-form")
